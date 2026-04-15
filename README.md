16dee562b1cb899145e6c033596e6b405ce493e7
Shubh Sapaliga <shubhsapaliga@gmail.com>
2026-04-15 21:38:55 +0530

    AI chatbot using Gemini API

---
+++ README.md
@@ -0,0 +1,22 @@
+# AI Chatbot using Gemini API
+
+This is a simple AI chatbot built using Python and Google Gemini API.
+
+## Features
+
+* Conversational AI
+* Chat history (context-aware responses)
+* Terminal based chatbot
+* Secure API key using `.env`
+* Error handling using `try` and `except` to prevent crashes
+
+## Tech Stack
+
+* Python
+* Google Gemini API
+* VS Code
+
+## Run Project
+
+pip install -r requirements.txt
+python chatbot.py
---
+++ chatbot.py
@@ -0,0 +1,45 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Get API key
+api_key = os.getenv("GOOGLE_API_KEY")
+
+# Create Gemini client
+client = genai.Client(api_key=api_key)
+
+print("AI Chatbot Started (type 'exit' to stop)\n")
+
+# Store conversation history
+chat_history = ""
+
+while True:
+
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        print("Chat ended.")
+        break
+
+    # Add user message to history
+    chat_history += f"You: {user_input}\n"
+
+    try:
+        # Send conversation to Gemini
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=chat_history
+        )
+
+        bot_reply = response.text
+
+        print("Bot:", bot_reply)
+
+        # Add bot reply to history
+        chat_history += f"Bot: {bot_reply}\n"
+
+    except Exception as e:
+        print("Error:", e)
